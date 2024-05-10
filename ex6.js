adf18d69b7c589a6cd8c679a0535752b2a3bcb0f
FELIPE FALCONI DE AZEVEDO <56958520807@info.local>
2024-05-10 08:54:34 -0300

    feat: exercicio 6

---
+++ ex6.js
@@ -0,0 +1,13 @@
+// Escreva um programa que solicita o ano de nascimento de uma pessoa e verifica se ela é maior
+// de 18 anos. Exiba uma mensagem indicando se a pessoa é maior de idade ou não.
+
+let aniversario = prompt("Digite o seu ano de nascimento")
+
+
+if (aniversario<2006) {
+    console.log ("Você é maior de idade")
+}
+
+else {
+    console.log ("Você é menor de idade")
+}

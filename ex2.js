73be84a325b38d6ee4bfc988fc49da159fde69a1
FELIPE FALCONI DE AZEVEDO <56958520807@info.local>
2024-05-10 08:25:35 -0300

    feat: exercicio 2

---
+++ ex2.js
@@ -0,0 +1,33 @@
+// Escreva um programa que solicita ao usuário um número de 1 a 7 representando um dia da
+// semana e exibe o nome correspondente a esse dia. Considere que 1 representa segunda-feira,
+// 2 representa terça-feira e assim por diante.
+
+
+let number = prompt("Olá, escolha um dia de 1 a 7")
+if (number == 1) {
+    console.log("Segunda-feira")
+}
+
+if (number == 2) {
+    console.log("Terça-feira")
+}
+
+if (number == 3) {
+    console.log("Quarta-feira")
+}
+
+if (number == 4) {
+    console.log("Quinta-feira")
+}
+
+if (number == 5) {
+    console.log("Sexta-feira")
+}
+
+if (number == 6) {
+    console.log("Sábado")
+}
+
+if (number == 7) {
+    console.log("Domingo")
+}

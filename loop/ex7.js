221302194aa9129d787e0c3ca72b8e3b4178b532
FELIPE FALCONI DE AZEVEDO <56958520807@info.local>
2024-05-14 10:55:23 -0300

    feat: exercicio 7

---
+++ loop/ex7.js
@@ -0,0 +1,9 @@
+//7. Calcular a soma dos números de 1 a 100 utilizando a estrutura while.
+
+let numero = parseInt(prompt("Indique um número de 1 a 100"));
+
+let numero = 0
+
+
+console.log ("Números pares de 0 a 20")
+while (numero <= 20) 

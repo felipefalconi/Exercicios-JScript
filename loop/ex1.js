2aab269eb443036bc5ba8349fa7a6fdf8746e52b
FELIPE FALCONI DE AZEVEDO <56958520807@info.local>
2024-05-14 08:42:53 -0300

    feat: exercicio 1 loop

---
+++ loop/ex1.js
@@ -0,0 +1,6 @@
+///1. Imprimir números de 1 a 10 utilizando o loop for
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
---
+++ loop/exemplo.js
@@ -0,0 +1,31 @@
+// let aumento = 1000;
+
+// do {
+//     console.log ("O salário ainda é R$" + aumento);
+
+//     aumento += 100
+// } while (aumento < 5000)
+
+
+// let saldo = 1000;
+
+// do {
+//     console.log("Esse é o saldo:" + saldo)
+//     saldo += 100;
+
+// } while (saldo < 5000)
+
+
+
+//Array 
+
+// let fruta = ["Banana", "Laranja", "Maça", "Pitaya", "Melancia"]
+
+// console.log(frutas[3])
+
+for(let indice = 0; indice < frutas.lenght; indice++){
+    console.log(frutas[indice]);
+    if(frutas[indice] == "Pitaya"){
+        console.log("Eu gosto de ")
+    }
+}

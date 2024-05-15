366234b75e2587c5bee0b11366b825e9648f940d
FELIPE FALCONI DE AZEVEDO <56958520807@info.local>
2024-05-15 10:56:39 -0300

    feat: exemplo funcoes

---
+++ funcoes/exemploFuncoes.js
@@ -0,0 +1,33 @@
+// function saudacao() {
+//     let nomePessoa = prompt ("Digite seu nome:")
+
+//     let horaAtual = new Date() .getHours()
+    
+//     if (horaAtual > 6 && horaAtual < 13) {
+//         window.alert("Bom dia " + nomePessoa)
+//     } else if (horaAtual >= 13 && horaAtual < 18) {
+//         window.alert("Boa tarde " + nomePessoa)
+//     } else {
+//         window.alert("Boa noite " + nomePessoa)
+//     } 
+// }
+
+// saudacao()
+
+
+// function somar(num1, num2, num3) {
+//     let soma = num1 + num2 + num3
+
+//     return soma
+// }
+
+// let resultado = somar(5, 16, 19)
+// let resultado2 = somar(1,1,1)
+
+// console.log ("O resultado é: " + resultado);
+
+let professor = {
+    nome: "Thiago",
+    sobrenome: "Nascimento",
+    idade: 25
+}
